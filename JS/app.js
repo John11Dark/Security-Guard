7a76828849cce373e0fa1dbd65a6bd00c6fa979d
john11dark <godsaveme2001@gmail.com>
2022-03-22 15:06:30 +0100

    first commit stage 1

---
+++ JS/app.js
@@ -0,0 +1,37 @@
+let befScrollTop = 0;
+let headerOpacity = 1;
+const Header = document.querySelector('header');
+
+// rotate logo circuit Board on scroll 
+window.onscroll = ('onscroll', () => {
+
+    let curScrollTop = window.scrollY || document.documentElement.scrollTop;
+    // on header hover 
+
+    if(curScrollTop <= 100){
+        Header.setAttribute("isInteracting", true);
+    }
+    else{
+        if(curScrollTop <= 100 ){
+        Header.setAttribute("isInteracting", false);
+        }
+    }
+    if (befScrollTop < curScrollTop) {
+        headerOpacity = 0.5;
+    }
+    else {
+        headerOpacity = 1;
+    }
+
+    befScrollTop = curScrollTop;
+    Header.style.opacity = headerOpacity;
+
+
+    Header.addEventListener("mouseover", () => {
+        Header.style.opacity = (headerOpacity + 0.5);
+
+        setTimeout(() => {
+            Header.style.opacity = headerOpacity;
+        }, 500);
+    }, false);
+});
